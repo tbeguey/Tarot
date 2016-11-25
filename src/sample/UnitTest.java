298065c8e5c8b8a82b333026f22828bb331b9caf
tricha <yamin.tricha@etu.u-bordeaux.fr>
2016-11-25 18:15:27 +0100

    cc

---
+++ src/sample/UnitTest.java
@@ -0,0 +1,28 @@
+package sample;
+
+import org.junit.Test;package Jui
+
+
+
+/**
+ * Created by oka on 25/11/16.
+ */
+public class UnitTest {
+
+    @Test
+    public void addCardPlayer()
+    {
+        Player p = new Player();
+        Card c = new Card();
+        assert ();
+
+    }
+
+    @Test
+    public void addCarteDog()
+    {
+
+    }
+
+    @Test
+}
